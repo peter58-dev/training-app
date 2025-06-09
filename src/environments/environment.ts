2cceb66f38e0c7464839b4dd43ed78bd34361f0d
peter-lelkes <p.lelkes@gmail.com>
2025-06-09 19:06:07 +0200

    firebase init + firebaseConfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCYCOMRjP5uw05meHp0S3TDPQjaSl2LPKs',
+
+    authDomain: 'strength-app-cd974.firebaseapp.com',
+
+    projectId: 'strength-app-cd974',
+
+    storageBucket: 'strength-app-cd974.firebasestorage.app',
+
+    messagingSenderId: '588939477744',
+
+    appId: '1:588939477744:web:16a9334867c35fde51f835',
+  },
 };
 
 /*
