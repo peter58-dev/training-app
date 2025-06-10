b668bbad9a091a9f8047285f08980ac243d31807
peter-lelkes <p.lelkes@gmail.com>
2025-06-10 22:57:53 +0200

    provideFirebaseApp() + provideFirestore(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+//import firebaseConfig
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(environment.firebase)), // tells angular how to get Firebase
+    provideFirestore(() => getFirestore()), // tells angular how to get Firestore
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
