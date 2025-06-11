c2f2fe7d5b84523d46feaa9dc328615ee4156202
peter-lelkes <p.lelkes@gmail.com>
2025-06-11 11:58:47 +0200

    PauseCountdownComponent created

---
+++ src/app/components/pause-countdown/pause-countdown.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-pause-countdown',
+  templateUrl: './pause-countdown.component.html',
+  styleUrls: ['./pause-countdown.component.scss'],
+})
+export class PauseCountdownComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
