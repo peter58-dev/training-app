af3f80253d390f283420453dbb451f10ff897d3c
Peter Lelkes <peter.lelkes@outlook.com>
2025-06-17 22:36:56 +0200

    new-program component created

---
+++ src/app/components/new-program/new-program.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-program',
+  templateUrl: './new-program.component.html',
+  styleUrls: ['./new-program.component.scss'],
+})
+export class NewProgramComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
