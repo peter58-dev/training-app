f4ae2d24c9671a03492cc28bd3fdee0d2070771e
Peter Lelkes <peter.lelkes@outlook.com>
2025-06-17 11:29:14 +0200

    unsubscribeProgramListener:Unsubscribe

--- src/app/services/app-service.service.ts
+++ src/app/services/app-service.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { Unsubscribe } from 'firebase/firestore';
 
+export interface TrainingProgram {
+  id: string //autogenererad,
+  namn:string
+}
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
-  constructor() {}
+// signal to store list of training programs
+ public trainingPrograms: WritableSignal<TrainingProgram[]>=signal([])
+
+  // variable for unsubscribe function for real-time listening
+  private unsubscribeProgramListener:Unsubscribe | null = null
+
+
+  constructor(private firestore:Firestore) {}
 }
