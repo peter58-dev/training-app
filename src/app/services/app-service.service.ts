79214f627673a3f923c281486fd297d3000ac532
Peter Lelkes <peter.lelkes@outlook.com>
2025-06-18 22:30:47 +0200

    firebase actioner tillagda

--- src/app/services/app-service.service.ts
+++ src/app/services/app-service.service.ts
@@ -1,5 +1,5 @@
-import { inject, Injectable } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { inject, Injectable, signal } from '@angular/core';
+import { addDoc, collection, deleteDoc, doc, Firestore, onSnapshot, updateDoc } from '@angular/fire/firestore';
 
 export interface TrainingProgram {
   id: string //autogenererad,
@@ -11,5 +11,39 @@ export interface TrainingProgram {
 export class AppService {
 private firestore = inject(Firestore);
 
-  constructor() {}
+  // Signal som innehåller listan
+  trainingPrograms = signal<any[]>([]);
+
+  constructor() {
+       const colRef = collection(this.firestore, 'trainingPrograms');
+
+    // Firebase lyssnar i realtid och uppdaterar signalen direkt
+    onSnapshot(colRef, (snapshot) => {
+      const result = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      this.trainingPrograms.set(result);
+    });
+  }
+
+   async addProgram(namn: string) {
+    const colRef = collection(this.firestore, 'trainingPrograms');
+    await addDoc(colRef, {
+      namn,
+      createdAt: new Date(),
+    });
+  }
+
+  async deleteProgram(id: string) {
+    const docRef = doc(this.firestore, 'trainingPrograms', id);
+    await deleteDoc(docRef);
+  }
+
+  async updateProgram(id: string, namn: string) {
+    const docRef = doc(this.firestore, 'trainingPrograms', id);
+    await updateDoc(docRef, { namn });
+  }
 }
+
+
