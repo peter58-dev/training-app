a98c7ab41f669046dbc1469beb46ede36d4c1463
Peter Lelkes <peter.lelkes@outlook.com>
2025-06-18 23:49:57 +0200

    ReactiveFormsModule

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HomePage } from './home.page';
 
@@ -13,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [HomePage,NewProgramComponent ]
 })
