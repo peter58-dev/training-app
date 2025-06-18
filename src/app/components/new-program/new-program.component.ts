81483ab7edca572bd9f438880b5deabd830ac0cf
Peter Lelkes <peter.lelkes@outlook.com>
2025-06-18 23:55:40 +0200

    newWorkoutForm;

--- src/app/components/new-program/new-program.component.ts
+++ src/app/components/new-program/new-program.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -17,7 +17,11 @@ newWorkoutForm!: FormGroup;
 
 ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.newWorkoutForm=this.fb.group({
+      namn:['', Validators.required]
+    })
+  }
   cancel() {
     return this.modalCtrl.dismiss(null,'cancel')
 
