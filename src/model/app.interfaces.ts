8195ee0c17b8419815b9df366b9c6fa632b74578
peter-lelkes <p.lelkes@gmail.com>
2025-06-23 23:51:57 +0200

    app.interfaces

---
+++ src/model/app.interfaces.ts
@@ -0,0 +1,18 @@
+export interface WorkoutProgram {
+  id: string;
+  name: string;
+  exercises: Exercise[];
+}
+
+export interface Exercise {
+  id: string;
+  name: string;
+  sets?: Set[];
+}
+
+export interface Set {
+  setNumber: number;
+  weight: number;
+  reps: number;
+  discs?: string;
+}
