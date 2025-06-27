20d4f90f008fa57671243e9e73be0d16a4e7a387
peter58-dev <used.for.dev@gmail.com>
2025-06-27 23:29:21 +0200

    generated WorkoutPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'workout',
+    loadChildren: () => import('./pages/workout/workout.module').then( m => m.WorkoutPageModule)
   },
+
 ];
 
 @NgModule({
---
+++ src/app/pages/workout/workout.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone:false,
+  selector: 'app-workout',
+  templateUrl: './workout.page.html',
+  styleUrls: ['./workout.page.scss'],
+})
+export class WorkoutPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
